48da83bf08d91d97ba0816f612bc9d9ba0d29565
kbagga19 <baggakaran19@gmail.com>
2023-06-09 21:14:46 +0530

    third commit

--- src/TrainPage.jsx
+++ src/TrainPage.jsx
@@ -8,7 +8,7 @@ function TrainPage() {
     const [trainDetail, setTrainDetail] = useState(null);
     const {trainNumber} = useParams();  
 
-    const headers = { 'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODYzMjUyMDUsImNvbXBhbnlOYW1lIjoiRGVsaGkgQ2VudHJhbCIsImNsaWVudElEIjoiZWM3MDYxMjctODBjZi00ZWY2LTkzNDQtY2M5NDZhNmYyZGZmIiwib3duZXJOYW1lIjoiIiwib3duZXJFbWFpbCI6IiIsInJvbGxObyI6IjA3MjEzMjAyNzIwIn0.pCC2XD_DpN-zbOezut00aN8cnG2F3AOWKVkDE9NTUWo'};
+    const headers = { 'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2ODYzMjU1MTYsImNvbXBhbnlOYW1lIjoiRGVsaGkgQ2VudHJhbCIsImNsaWVudElEIjoiZWM3MDYxMjctODBjZi00ZWY2LTkzNDQtY2M5NDZhNmYyZGZmIiwib3duZXJOYW1lIjoiIiwib3duZXJFbWFpbCI6IiIsInJvbGxObyI6IjA3MjEzMjAyNzIwIn0.1fau66xg-57qzD5-EnNk3Oeh1HUjBCsYp33iW_b4JDg'};
 
   useEffect(() => {
     fetch(`http://104.211.219.98/train/trains/${trainNumber}`, {headers})
@@ -25,7 +25,11 @@ function TrainPage() {
   return (
     <div className="App">
         <div className='trains_data'>
-            <h2>{trainDetail.trainName}</h2>
+        <h1 className="train_name">Train Name: {trainDetail.trainName}</h1>
+        <h3>Train Number: {trainDetail.trainNumber}</h3>
+        <h5>Deaparture Time: {trainDetail.departureTime.Hours}:{trainDetail.departureTime.Minutes}:{trainDetail.departureTime.Seconds}</h5>
+        <h5>Delayed By: {trainDetail.delayedBy}</h5>
+
         </div>
     </div>
   );
